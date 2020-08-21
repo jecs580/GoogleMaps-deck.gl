c952a615ee044f58a32f7eac9d6d3906dccbe44b
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-08-20 22:53:03 -0400

    Agregando un botton con estilos por cada pais obtenido

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 const $map = document.getElementById('map'); // Colocamos con un $ adelante a los selectores.
-const EUROPE_CENTER={lat: 47.582798, lng: 9.707756};
+const $controls =document.getElementById('controls');
+const EUROPE_CENTER = {lat: 47.582798, lng: 9.707756};
 let GMAP;
-function initMap() {
-
-    GMAP = new google.maps.Map($map, {
-        zoom: 4,
-        center: EUROPE_CENTER,
-      });
+function updateMap() {
+  console.log(this.textContent);
 }
-google.maps.event.addDomListener(window,'load',initMap);
+
+function renderButtonElement(country) {
+  const element = document.createElement('button')
+  element.textContent = country
+  element.addEventListener('click',updateMap)
+  $controls.appendChild(element)
+}
+async function renderButtons() {
+  const response = await fetch(`./catalogs/countries.json`)
+  const countries = await response.json()
+  countries.forEach(item =>renderButtonElement(item));
+}
+
+function initMap() {
+  GMAP = new google.maps.Map($map, {
+    zoom: 4,
+    center: EUROPE_CENTER
+  });
+  renderButtons()
+}
+google.maps.event.addDomListener(window,'load', initMap);
