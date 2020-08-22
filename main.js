ccf4b41a2daf80f13bbaeac5eb914c734f7c84f5
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-08-21 21:12:55 -0400

    Usando Deck.gl para renderizar un punto por cada arbol

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 const $map = document.getElementById('map'); // Colocamos con un $ adelante a los selectores.
 const $controls =document.getElementById('controls');
 const EUROPE_CENTER = {lat: 47.582798, lng: 9.707756};
-let GMAP;
-function updateMap() {
-  console.log(this.textContent);
+const {ScatterplotLayer,GoogleMapsOverlay}=deck;
+let GMAP, DECKGL_OVERLAY;
+async function updateMap() {
+  let country=this.textContent.replace(/ /g,'').toLowerCase();
+  let layer= await getLayer(country);
+  DECKGL_OVERLAY.setProps({layers:[await layer]})
 }
-
+async function getLayer(country='austria') {
+  // Pinta de un punto en la posicion de cada arbol
+  const request_country= await fetch(`./data/${country}.json`);
+  const data_country = await request_country.json();
+  return await new ScatterplotLayer({
+    id:'trees',
+    data:data_country,
+    radiusMinPixels:1,
+    radiusMaxPixels:100,
+    lineWidthMinPixels:1,
+    getPosition:d=>[d.lng,d.lat],
+    getRadius:d=>50,
+    gerFillColor:d=>[0,155,115],
+    getLineColor:d=>[30,30,30]
+  });
+}
 function renderButtonElement(country) {
   const element = document.createElement('button')
   element.textContent = country
@@ -18,11 +36,14 @@ async function renderButtons() {
   countries.forEach(item =>renderButtonElement(item));
 }
 
-function initMap() {
+async function initMap() {
   GMAP = new google.maps.Map($map, {
     zoom: 4,
     center: EUROPE_CENTER
   });
+  DECKGL_OVERLAY= new GoogleMapsOverlay();  // Indicamos que genere una capa
+  DECKGL_OVERLAY.setMap(GMAP); // Indicamos que la capa se colocara encima de GoogleMa p
+  DECKGL_OVERLAY.setProps({layers: [await getLayer()]})
   renderButtons()
 }
 google.maps.event.addDomListener(window,'load', initMap);
